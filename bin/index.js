02d2c018a3482d11249d0272e4425239efa00edd
steelydylan <ess_president@me.com>
2018-05-20 11:33:27 +0900

    added default option

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 "use strict";
 
+const extract = require('./extract');
+
 const argv = require('yargs')
-  .command('extract', 'extract classnames from a html file', require('./extract'))
+  .command('*','extract classnames from a html file', extract)
+  .command('extract', 'extract classnames from a html file', extract)
   .detectLocale(false)
   .version(function() { return require('../package').version; })
   .alias('v', 'version')
